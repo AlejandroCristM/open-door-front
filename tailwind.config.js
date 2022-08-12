d849616c7743e46c37ff5d955576e031e2dadf7b
Alejandro Cristancho M <alejandro.cristanchom@gmail.com>
2022-08-12 16:10:21 -0500

    chore: tailwind, rrd, react icons installed and folder structure stablished

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        sm: '320px',
+        md: '768px',
+        lg: '1024px',
+      },
+      color:{
+        gray: '#E5E5E5',
+        'blue-lt': '#140455',
+        'orange-lt': '#FF4E00',
+        'grey-lt': '#282F4B'
+      } 
+    },
+  },
+  plugins: [],
+}
