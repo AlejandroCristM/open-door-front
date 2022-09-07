20ba62c58aab2cc9fa6bb962beb263755e63f78e
Alejandro Cristancho M <alejandro.cristanchom@gmail.com>
2022-09-07 10:02:46 -0500

    feat: auth, home page, not found 404, Unauthorized page added

--- src/components/atoms/ButtonAndIcon.jsx
+++ src/components/atoms/ButtonAndIcon.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-export default function ButtonAndIcon({icon, text, otherStyles, responsive}) {
+export default function ButtonAndIcon({icon, text, otherStyles, responsive, onClick}) {
 
   let responsiveStyle=''
   responsive ? responsiveStyle=' hidden md:inline' : responsiveStyle='inline';
 
+
+
   return (
-    <button className={'flex flex-row items-center rounded p-2 md:space-x-1 '+otherStyles}>
+    <button onClick={onClick} className={'flex flex-row items-center rounded p-2 md:space-x-1 '+otherStyles}>
       <span className={responsiveStyle}>{text}</span>
       {icon}
     </button>
--- src/components/molecules/NavBar.jsx
+++ src/components/molecules/NavBar.jsx
@@ -1,14 +1,15 @@
 import React, {useState} from 'react'
 import openDoorLogo from '../../assets/openDoorIcon.svg'
-import { BiExit } from "react-icons/bi";
-import ButtonAndIcon from '../../components/atoms/ButtonAndIcon'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import LoginButton from '../atoms/LoginButton';
+import LogOutButton from '../atoms/LogOutButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 export default function NavBar() {
-
+  const {isAuthenticated} = useAuth0();
   const [navbarOpen, setNavbarOpen] = useState(false)
 
   const handleMenuToggle = () => {
@@ -18,25 +19,33 @@ export default function NavBar() {
   return (
     <nav className='flex flex-row w-full mt-5 px-5 py-5 justify-between items-center md:px-10 lg:px-20'>
         <div className='absolute top top-16 left-8'>
-        {navbarOpen? <DropMenu toggle={handleMenuToggle}/>: null}
+        {navbarOpen && isAuthenticated? <DropMenu toggle={handleMenuToggle}/>: null}
         </div>
-        <section className='flex flex-row items-center'>
-            <div className='mr-5' onClick={handleMenuToggle}>
-              {navbarOpen?
-                <MdClose className='h-5 w-5 text-orange-lt md:hidden' />:
-                <GiHamburgerMenu className='h-5 w-5 text-black md:hidden' />
-              }
-            </div>
-            <Link to={'/'}>
-              <img src={openDoorLogo} alt="Open Door Logo" />
-            </Link>
-            <div className='flex flex-row space-x-10 ml-8 list-none hidden text-blue-lt md:flex'>
-              <Link to={'/profile'} className='navItem'>Mi perfil</Link>
-              <Link to={'/tracking'} className='navItem'>Seguimiento</Link>
-              <Link to={'/courses'} className='navItem'>Cursos</Link>
-            </div>
-        </section>
-        <ButtonAndIcon icon={<BiExit className='h-5 w-5'/>} text='Salir' otherStyles='bg-orange-lt text-white' responsive={true} />
+        {isAuthenticated?
+          <section className='flex flex-row items-center'>
+              <div className='mr-5' onClick={handleMenuToggle}>
+                {navbarOpen?
+                  <MdClose className='h-5 w-5 text-orange-lt md:hidden' />:
+                  <GiHamburgerMenu className='h-5 w-5 text-black md:hidden' />
+                }
+              </div>
+              <Link to={'/'}>
+                <img src={openDoorLogo} alt="Open Door Logo" />
+              </Link>
+              <div className='flex flex-row space-x-10 ml-8 list-none hidden text-blue-lt md:flex'>
+                <Link to={'/profile'} className='navItem'>Mi perfil</Link>
+                <Link to={'/tracking'} className='navItem'>Seguimiento</Link>
+                <Link to={'/courses'} className='navItem'>Cursos</Link>
+              </div>
+          </section>
+          :
+          <section className='flex flex-row items-center'>
+              <Link to={'/'}>
+                <img src={openDoorLogo} alt="Open Door Logo" />
+              </Link>
+          </section>
+        }
+        {isAuthenticated? <LogOutButton/> : <LoginButton/>}
     </nav>
   )
 }
